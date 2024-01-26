aabf62934e24684086dcbe568603b12a4d55e8ed
Vedant <mahanavarvedant7049@gmail.com>
2024-01-26 11:30:10 +0530

    uh idk j

--- django_project/settings.py
+++ django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"], #New
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
